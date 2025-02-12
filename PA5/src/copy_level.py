78cc67ac9bece960ba8a8e0d88c188f4a8f1fdcb
carlosnuno1 <150867782+carlosnuno1@users.noreply.github.com>
2025-02-11 21:11:20 -0800

    playable levels

--- PA5/src/copy_level.py
+++ PA5/src/copy_level.py
@@ -5,7 +5,7 @@ import os
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         path = sys.argv[1]
-    else:
+    else:   
         path = "levels/last.txt"
     if not os.path.exists(path):
         print("The file at " + path + " does not exist.")
